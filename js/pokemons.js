eba002254c6b41155c4996eb2571f5a38fd89652
BelaSchrammDev <belaschramm@aol.de>
2023-12-28 22:03:42 +0100

    set types and colors

--- js/pokemons.js
+++ js/pokemons.js
@@ -1,7 +1,29 @@
 let all_PokeMons = [];
 let allPokemonJsons = {};
 let lastShowPokemon = 0;
+let big_CardPositionRect = null;
 const loadCount = 30;
+const typeColors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+};
+
 
 
 async function initPokemons() {
@@ -21,12 +43,37 @@ function addLoadByScrollBehavior() {
 
 async function renderSinglePokemon(pokemonName) {
     const pokemon = await getPokemonObject(pokemonName);
-    document.getElementById(pokemonName).innerHTML += `
-    <div>
-        <span> Name: ${pokemon['name']}</span>
-        <img src="${pokemon['sprites']['front_default']}">
-    </div>
+    const card = document.getElementById(pokemonName);
+    card.innerHTML += getPokemonCardHTML(pokemon);
+    card.style = `background-color: ${getPokemonMainTypeColor(pokemon)}`;
+}
+
+
+function getPokemonCardHTML(pokemon) {
+    return `
+        <div class="flex_c_jfs_ast flex_gap_2">
+            <img class="pokemon_image" src="${pokemon['sprites']['front_default']}">
+            <span class="pokemon_name font_24b">${pokemon['name']}</span>
+            ${getPokemonTypesHTML(pokemon)}
+        </div>
     `;
+}
+
+
+function getPokemonMainTypeColor(pokemon) {
+    const firstType = pokemon.types[0].type;
+    if (firstType && typeColors.hasOwnProperty(firstType.name)) return typeColors[firstType.name];
+    else typeColors.normal;
+}
+
+
+function getPokemonTypesHTML(pokemon) {
+    let typesHTML = '';
+    for (let index = 0; index < pokemon.types.length; index++) {
+        const type = pokemon.types[index].type;
+        typesHTML += `<span>${type.name}</span>`;
+    }
+    return typesHTML;
 }
 
 
@@ -50,7 +97,23 @@ async function renderNextPokemons() {
 
 
 function getNewEmptyCard(pokemonID) {
-    return `<div class="pokemon_smallcard"><div id="${pokemonID}"></div></div>`
+    return `
+        <div id="div_${pokemonID}" onclick="clickPokemonSmallCard('${pokemonID}')" class="pokemon_smallcard">
+            <div class="pokemon_innercard" id="${pokemonID}"></div>
+        </div>
+        `;
+}
+
+
+function clickPokemonSmallCard(pokemonID) {
+    stopPageScrolling();
+    const card = document.getElementById(pokemonID);
+}
+
+
+function clickOverlay() {
+    const card = document.getElementById('overlay').children[0];
+    allowPageScrolling();
 }
 
 
