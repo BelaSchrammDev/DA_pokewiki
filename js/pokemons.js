ecf98e13279f5243703105dae0b19b2b47defbcb
BelaSchrammDev <belaschramm@aol.de>
2024-01-03 19:10:33 +0100

    first styleturn of pokemon card

--- js/pokemons.js
+++ js/pokemons.js
@@ -2,7 +2,7 @@ let all_PokeMons = [];
 let allPokemonJsons = {};
 let lastShowPokemon = 0;
 let big_CardPositionRect = null;
-const loadCount = 30;
+const loadCount = 40;
 const typeColors = {
     normal: '#A8A77A',
     fire: '#EE8130',
@@ -25,10 +25,9 @@ const typeColors = {
 };
 
 
-
 async function initPokemons() {
     await loadPokemonList()
-    renderNextPokemons();
+    renderFirstPokemons();
     addLoadByScrollBehavior();
 }
 
@@ -36,26 +35,29 @@ async function initPokemons() {
 function addLoadByScrollBehavior() {
     window.addEventListener('scroll', () => {
         const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-        if (lastShowPokemon < all_PokeMons.length && (scrollTop + clientHeight) >= scrollHeight - 200) renderNextPokemons();
+        if (lastShowPokemon < all_PokeMons.length && (scrollTop + clientHeight) >= scrollHeight - 100) renderNextPokemons();
     });
 }
 
 
-async function renderSinglePokemon(pokemonName) {
-    const pokemon = await getPokemonObject(pokemonName);
-    const card = document.getElementById(pokemonName);
+async function renderSinglePokemonByID(pokemonID) {
+    const pokemon = await getPokemonObjectByID(pokemonID);
+    const card = document.getElementById(pokemonID.name);
     card.innerHTML += getPokemonCardHTML(pokemon);
     card.style = `background-color: ${getPokemonMainTypeColor(pokemon)}`;
 }
 
 
+function getPascalCaseWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+
 function getPokemonCardHTML(pokemon) {
     return `
-        <div class="flex_c_jfs_ast flex_gap_2">
-            <img class="pokemon_image" src="${pokemon['sprites']['front_default']}">
-            <span class="pokemon_name font_24b">${pokemon['name']}</span>
-            ${getPokemonTypesHTML(pokemon)}
-        </div>
+        <img class="pokemon_image" src="${pokemon['sprites']['front_default']}">
+        <span class="pokemon_name font_24b flex_grow_1">${getPascalCaseWord(pokemon['name'])}</span>
+        ${getPokemonTypesHTML(pokemon)}
     `;
 }
 
@@ -73,25 +75,47 @@ function getPokemonTypesHTML(pokemon) {
         const type = pokemon.types[index].type;
         typesHTML += `<span>${type.name}</span>`;
     }
-    return typesHTML;
+    return `<div class="${pokemon.types.length > 1 ? 'flex_r_jsb_ace' : 'flex_r_jfs_ace'}">${typesHTML}</div>`;
 }
 
 
 async function loadPokemonList() {
-    const url = `https://pokeapi.co/api/v2/pokemon-species/?limit=10000`;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
     all_PokeMons = responseAsJson.results;
 }
 
 
-async function renderNextPokemons() {
+function renderFirstPokemons() {
+    lastShowPokemon = 0; // start by first pokemon
+    document.getElementById('main_content').innerHTML = '';
+    renderNextPokemons();
+}
+
+
+function renderNextPokemons() {
+    if (lastShowPokemon == -1) return; // pokemonsearch is active
     const loadBegin = lastShowPokemon;
     const loadEnd = lastShowPokemon + loadCount;
     lastShowPokemon = loadEnd;
     for (let i = loadBegin; i < all_PokeMons.length && i < loadEnd; i++) {
         document.getElementById('main_content').innerHTML += getNewEmptyCard(all_PokeMons[i].name);
-        renderSinglePokemon(all_PokeMons[i].name);
+        renderSinglePokemonByID(all_PokeMons[i]);
+    }
+}
+
+
+function renderPokemonsByFilter(filter) {
+    lastShowPokemon = -1; // stop loading when scroll to botton
+    filter = filter.toLowerCase();
+    document.getElementById('main_content').innerHTML = '';
+    for (let i = 0; i < all_PokeMons.length; i++) {
+        const pokemonName = all_PokeMons[i].name;
+        if (pokemonName.includes(filter)) {
+            document.getElementById('main_content').innerHTML += getNewEmptyCard(pokemonName);
+            renderSinglePokemon(all_PokeMons[i]);
+        }
     }
 }
 
@@ -117,11 +141,11 @@ function clickOverlay() {
 }
 
 
-async function getPokemonObject(pokemonName) {
-    if (!allPokemonJsons[pokemonName]) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-        let response = await fetch(url);
-        allPokemonJsons[pokemonName] = await response.json();
+async function getPokemonObjectByID(pokemonID) {
+    if (!allPokemonJsons[pokemonID.name]) {
+        let response = await fetch(pokemonID.url);
+        allPokemonJsons[pokemonID.name] = await response.json();
     }
-    return allPokemonJsons[pokemonName];
+    return allPokemonJsons[pokemonID.name];
 }
+
